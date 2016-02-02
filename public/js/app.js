92c4dd45333929a8971084a034e205c051b23ebc
Chase Gruber <chasegruber@gmail.com>
2016-02-01 20:02:00 -0500

    fix cluster labels

--- public/js/app.js
+++ public/js/app.js
@@ -9,10 +9,12 @@ new L.Control.Zoom({ position: 'topright' }).addTo(map);
 
 var ptCluster = new L.MarkerClusterGroup({
     iconCreateFunction: function (cluster) {
+      var count = cluster.getChildCount();
+      var text = (count>100)?'100+':(count>250)?'250+':(count>500)?'500+':(count>750)?'750+':(count>1000)?'1k+':(count>1500)?'1.5k+':(count>2000)?'2k+':count;
         return L.divIcon({
-          html:cluster.getChildCount(),
+          html:text,
           className: 'cluster',
-          iconSize:L.point(25,25)
+          iconSize:L.point(30,30)
         })
     },
     spiderfyOnMaxZoom: false,
